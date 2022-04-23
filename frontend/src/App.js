dcc2e9cf4ad0f15421559d0b75658f2e417aa318
TuanKietTran <kyletran101.work@gmail.com>
2022-04-23 14:14:05 +0700

    add redux and router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,13 @@
 import './App.scss';
 import './App.css';
-import { Button } from 'react-bootstrap';
+import { Routes, Route } from "react-router-dom";
 
-import Map from './components/Map'
+import Running from './pages/running/Running';
 function App() {
-  return (  
-    <div>
-      <Button variant="primary">Primary</Button>{' '}
-  <Button variant="secondary">Secondary</Button>{' '}
-  <Button variant="success">Success</Button>{' '}
-  <Button variant="warning">Warning</Button>{' '}
-  <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
-  <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
-  <Button variant="link">Link</Button>
-    </div>
+  return (
+    <Routes>
+      <Route path="/running" element={<Running />} />
+    </Routes>
   );
 }
 
