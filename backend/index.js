be5917258b8f3d87c84cd935cea7cd8eb1536fb8
TuanKietTran <kyletran101.work@gmail.com>
2022-04-23 14:08:13 +0700

    add react-router and redux

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+
+
+const db = {
+  profiles: [
+    {
+      name: 'Cu Do Thanh Nhan',
+      account: 'cudothanhnhan',
+      tree: 1024,
+      badges: 'Rookie',
+      routes: [
+        'Tan Lap',
+        'Vanh Dai DHQG',
+        'Nguyen Du'
+      ]
+    }
+  ],
+  badges: [
+    {
+      name: 'Rookie',
+      threshold: 100,
+      img: ''
+    },
+    {
+      name: 'Eco',
+      threshold: 1000,
+      img: ''
+    },
+    {
+      name: 'Green-ish',
+      threshold: 100000,
+      img: ''
+    },
+  ],
+  events: [
+    {
+      name: 'Green Day',
+      stakeholder: 'atWare',
+      start: '26/4/2022',
+      end: '30/4/2022',
+      bonus: 1,
+      img: '',
+    }
+  ],
+  
+}
