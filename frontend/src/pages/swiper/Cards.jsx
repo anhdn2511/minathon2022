764bcfb9de99a211e6aee5e1b21a5476d05f214b
anhdn2511 <anhdn2511@gmail.com>
2022-04-24 03:33:53 +0700

    edit icon

--- frontend/src/pages/swiper/Cards.jsx
+++ frontend/src/pages/swiper/Cards.jsx
@@ -120,10 +120,20 @@ function Swiper () {
       </div>
       <div className="buttonSwipe col-md-10 justify-content-center">
         {/* <button style={{ backgroundColor: !canSwipe && '#c3c4d3' }} onClick={() => swipe('left')}>Swipe left!</button> */}
-        <button type="button" className="btn btn-danger rounded-circle buttonOfSwipe" onClick={() => showMessage('left')}>Deny</button>
+        <button type="button" className="btn btn-danger rounded-circle buttonOfSwipe" onClick={() => showMessage('left')}>
+
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+        </svg>
+        </button>
         {/* <button className="btn btn-primary rounded-circle buttonOfSwipe" style={{ backgroundColor: !canGoBack && '#c9bebd' }} onClick={() => goBack()}>Undo swipe!</button> */}
         {/* <button style={{ backgroundColor: !canSwipe && '#c3c4d3' }} onClick={() => swipe('right')}>Swipe right!</button> */}
-        <button type="button" className="btn btn-success rounded-circle buttonOfSwipe" onClick={() => showMessage('right')}>Accept</button>
+        <button type="button" className="btn btn-success rounded-circle buttonOfSwipe" onClick={() => showMessage('right')}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+          <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+          </svg>
+
+        </button>
       </div>
     </div>
   );
