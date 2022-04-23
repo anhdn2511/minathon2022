971b4add5da6df410217a2354d4b5d182b397011
anhdn2511 <anhdn2511@gmail.com>
2022-04-24 02:26:55 +0700

    create event

--- frontend/src/pages/event/CreateEvent.jsx
+++ frontend/src/pages/event/CreateEvent.jsx
@@ -1,19 +1,58 @@
-import React from 'react'
-import {Row, Container} from 'react-bootstrap'
+import React, {useEffect, useState} from 'react'
+import {Form, Button} from 'react-bootstrap'
 
 export default function CreateEvent() {
+    const fullEvent = {
+        title: "Longbien Marathon 2022 1st Ekiden",
+        imageUrl: "https://www.runtastic.com/blog/wp-content/uploads/2019/03/blog_thumbnail_run-for-the-oceans_proper-running-form_1200x800-1024x683.jpg",
+        organize: "Atware",
+        description: "Marathon Running for Charity",
+        startDate: '24 Apr 2022'
+    }
+
     return (
-    <div>
-      <div className='vh-25'>
-        <img className='px-0 border-radius-bottom h-100 w-100' src="https://www.runtastic.com/blog/wp-content/uploads/2019/03/blog_thumbnail_run-for-the-oceans_proper-running-form_1200x800-1024x683.jpg"/>
+        <div>
+        <div className="vh-25">
+          <div
+            className="px-0 h-50 w-100 bg-primary"
+          />
+        </div>
+        <div className='row justify-content-center align-items-start'>
+            <div className='col-6'>
+                <p className='h1'>Create Event</p>
+            </div>
+        </div>
+        <Form className='px-3'>
+            <Form.Group className="mb-3">
+                <Form.Label>Name</Form.Label>
+                <Form.Control placeholder="Enter Name" defaultValue={fullEvent.title}/>
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+                <Form.Label>Description</Form.Label>
+                <Form.Control placeholder="Enter description" defaultValue={fullEvent.description}/>
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+                <Form.Label>Organization Unit</Form.Label>
+                <Form.Control placeholder="Enter Organization Unit" defaultValue={fullEvent.organize}/>
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+                <Form.Label>Link image</Form.Label>
+                <Form.Control placeholder="Enter image url" defaultValue={fullEvent.imageUrl}/>
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+                <Form.Label>Start Date</Form.Label>
+                <Form.Control placeholder="Enter start date" defaultValue={fullEvent.startDate}/>
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+            
+        </Form>
       </div>
-      <div className='vh-50 px-5'>
-        <Row className='h-100 negative-mg-top-1'>
-          <Container className='white-background border border my-rounded shadow p-3 bg-body'>
-            <h1>Nhân Cù</h1>
-          </Container>
-        </Row>
-      </div>
-    </div>
     )
 }
