b96cd22494a130b133ecec6d8a757dff69055b92
anhdn2511 <anhdn2511@gmail.com>
2022-04-23 11:49:00 +0700

    define map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,10 @@
-import logo from './logo.svg';
 import './App.scss';
-import { Button } from 'react-bootstrap';
+import './App.css';
 
+import Map from './components/Map'
 function App() {
-  return (
-    <div className="App">
-  <Button variant="primary">Primary</Button>{' '}
-  <Button variant="secondary">Secondary</Button>{' '}
-  <Button variant="success">Success</Button>{' '}
-  <Button variant="warning">Warning</Button>{' '}
-  <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
-  <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
-  <Button variant="link">Link</Button>
-    </div>
+  return (  
+    <Map />
   );
 }
 
