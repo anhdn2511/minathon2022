18951481b41a8ce459bb580e9d6b0d9c2cb9931e
anhdn2511 <anhdn2511@gmail.com>
2022-04-23 20:45:08 +0700

    qanh push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,10 @@
 import './App.scss';
 import './App.css';
-import { Button } from 'react-bootstrap';
+import Swiper from './pages/swiper/Swiper'
 
-import Map from './components/Map'
 function App() {
   return (  
-    <div>
-      <Button variant="primary">Primary</Button>{' '}
-  <Button variant="secondary">Secondary</Button>{' '}
-  <Button variant="success">Success</Button>{' '}
-  <Button variant="warning">Warning</Button>{' '}
-  <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
-  <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
-  <Button variant="link">Link</Button>
-    </div>
+    <Swiper />
   );
 }
 
