154537dbd89241c9eb1ae39859a54257385e28e7
anhdn2511 <anhdn2511@gmail.com>
2022-04-23 12:39:11 +0700

    edit color scss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import './App.scss';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 import Map from './components/Map'
 function App() {
   return (  
-    <Map />
+    <div>
+      <Button variant="primary">Primary</Button>{' '}
+  <Button variant="secondary">Secondary</Button>{' '}
+  <Button variant="success">Success</Button>{' '}
+  <Button variant="warning">Warning</Button>{' '}
+  <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
+  <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
+  <Button variant="link">Link</Button>
+    </div>
   );
 }
 
